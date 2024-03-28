dc659cfb86d4e090fea7f2826d763f003c2f082b
Mahmoud <mah2002moud@gmail.com>
2024-03-29 01:20:28 +0200

    .

--- monty.c
+++ monty.c
@@ -1,68 +1,88 @@
 #include "monty.h"
 
+
 /**
- * main - Entry point
+ * main - Entry point for Monty bytecode interpreter
  * @argc: Argument count
  * @argv: Argument vector
- *
- * Description: Entry point for Monty bytecode interpreter.
  * Return: EXIT_SUCCESS on success, or EXIT_FAILURE on failure
  */
 int main(int argc, char **argv)
 {
-    FILE *file;
-    char *line = NULL;
-    size_t len = 0;
+	FILE *file;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	unsigned int line_number = 0;
+	stack_t *stack = NULL;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\\n");
+		exit(EXIT_FAILURE);
+	}
 
-    file = fopen(argv[1], "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	file = fopen(argv[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-    while (getline(&line, &len, file) != -1)
-    {
-        // Process the line
-    }
+	while ((read = getline(&line, &len, file)) != -1)
+	{
+		line_number++;
+		execute_line(line, &stack, line_number);
+	}
 
-    free(line);
-    fclose(file);
-    return 0;
+	free(line);
+	fclose(file);
+
+	/* Free stack */
+	while (stack != NULL)
+	{
+		stack_t *temp = stack;
+
+		stack = stack->next;
+		free(temp);
+	}
+
+	return (0);
 }
 
 /**
- * push - Pushes an element to the stack
+ * execute_line - Executes an opcode
+ * @line: Line from the file
  * @stack: Double pointer to the top of the stack
- * @line_number: Line number of the current operation
- *
- * Description: Pushes an integer onto the stack
+ * @line_number: Current line number
  */
-void push(stack_t **stack, unsigned int line_number)
+void execute_line(char *line, stack_t **stack, unsigned int line_number)
 {
-    stack_t *new_node;
+	char *opcode;
+	char *arg;
+	int num;
 
-    new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	opcode = strtok(line, " \n");
+	arg = strtok(NULL, " \n");
 
-    new_node->n = line_number; // Modify to get actual value
-    new_node->prev = NULL;
-    new_node->next = *stack;
+	if (opcode == NULL)
+		return;
 
-    if (*stack != NULL)
-        (*stack)->prev = new_node;
-
-    *stack = new_node;
+	/* Implement opcode handling here, like push, pall, etc. */
+	if (strcmp(opcode, "push") == 0)
+	{
+		num = atoi(arg);
+		if (arg == NULL || (arg[0] != '0' && num == 0))
+		{
+			fprintf(stderr, "L%d: usage: push integer\\n", line_number);
+			exit(EXIT_FAILURE);
+		}
+		push(stack, num);
+	}
+	else if (strcmp(opcode, "pall") == 0)
+	{
+		pall(stack, line_number);
+	}
 }
 
 /**
@@ -74,13 +94,41 @@ void push(stack_t **stack, unsigned int line_number)
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp = *stack;
+	stack_t *temp = *stack;
 
-    (void)line_number;
+	(void)line_number;
 
-    while (temp != NULL)
-    {
-        printf("%d\n", temp->n);
-        temp = temp->next;
-    }
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
+
+/**
+ * push - Pushes an element to the stack
+ * @stack: Double pointer to the top of the stack
+ * @line_number: Line number of the current operation
+ *
+ * Description: Pushes an integer onto the stack
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = line_number;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+
+	*stack = new_node;
 }
--- monty.h
+++ monty.h
@@ -38,5 +38,5 @@ typedef struct instruction_s
 /* Function prototypes */
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
-
+void execute_line(char *line, stack_t **stack, unsigned int line_number);
 #endif /* MONTY_H */
