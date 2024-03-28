5a8d378cca43e774929a0980dee637808f6465d3
Mahmoud <mah2002moud@gmail.com>
2024-03-29 01:28:12 +0200

    AlxProject

--- monty.h
+++ monty.h
@@ -36,7 +36,7 @@ typedef struct instruction_s
 } instruction_t;
 
 /* Function prototypes */
-void push(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, char *arg, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void execute_line(char *line, stack_t **stack, unsigned int line_number);
 #endif /* MONTY_H */
