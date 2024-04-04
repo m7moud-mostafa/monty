9132b43cf5245d317eb27848e6618b9165a06a9d
Mahmoud <mah2002moud@gmail.com>
2024-04-04 12:54:27 +0200

    monty_commends.c

--- monty_commends.c
+++ monty_commends.c
@@ -91,6 +91,7 @@ void swap(stack_t **stack, unsigned int line_number)
 void add(stack_t **stack, unsigned int line_number)
 {
 	int sum;
+
 	if (!stack || !(*stack) || !((*stack)->next))
 	{
 		fprintf(stderr, "L%i: can't add, stack too short\n", line_number);
