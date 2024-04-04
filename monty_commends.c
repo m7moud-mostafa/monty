367c956c483bac47d3fe6b09c816413948b03d3e
Mahmoud <mah2002moud@gmail.com>
2024-04-04 12:12:03 +0200

    monty_commends.c

--- monty_commends.c
+++ monty_commends.c
@@ -45,7 +45,7 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	if (!stack || !(*stack))
 	{
-		fprintf(stderr, "L%i: can't pop an empty stack", line_number);
+		fprintf(stderr, "L%i: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
