62e176c3d7b614f52fc242ba64a1ace326dff80c
Mahmoud <mah2002moud@gmail.com>
2024-04-04 12:38:55 +0200

    monty_commends.c

--- monty_commends.c
+++ monty_commends.c
@@ -82,3 +82,23 @@ void swap(stack_t **stack, unsigned int line_number)
 		tmp->next->prev = tmp;
 
 }
+
+/**
+ * add - add top two elements
+ * @stack: Double pointer to the top of the stack
+ * @line_number: Line number of the current operation
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	int sum;
+	if (!stack || !(*stack) || !((*stack)->next))
+	{
+		fprintf(stderr, "L%i: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	sum = (*stack)->n + (*stack)->next->n;
+	(*stack)->next->n = sum;
+	pop(stack, line_number);
+
+}
